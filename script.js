ba68865969d3a72f4af1f8dbb8dd69ad485f9d3f
Payman22 <140825344+Payman22@users.noreply.github.com>
2024-05-28 11:08:29 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+<script src="https://apis.google.com/js/api.js"></script>
+
+function initClient() {
+    gapi.client.init({
+        'apiKey': 'AIzaSyAkoryVtiY5zCVAYTlSquSmt578k4l46YI',
+        'clientId': '1050886216470-gpqrg654jt88mnkjuthdmll3t0p929k8.apps.googleusercontent.com',
+        'scope': 'https://www.googleapis.com/auth/spreadsheets',
+        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(function () {
+        // Your code to handle successful initialization
+    }, function(error) {
+        console.log(JSON.stringify(error, null, 2));
+    });
+}
+
+gapi.load('client:auth2', initClient);
+
 function handleRedButton() {
     alert("Red button clicked!");
 }
@@ -5,3 +22,28 @@ function handleRedButton() {
 function handleGreenButton() {
     alert("Green button clicked!");
 }
+
+function handleSignInClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignOutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+function readSheetData() {
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1gzFiV8qYD5EW4fSPd1NUP2mj7h9q0-UMwNoxUagMLEU',
+        range: 'Sheet1!A1:E10', // Adjust the range as needed
+    }).then(function(response) {
+        var range = response.result;
+        console.log(range);
+        // Handle the response data
+    }, function(error) {
+        console.error('Error: ' + error.result.error.message);
+    });
+}
+
+
+
+
