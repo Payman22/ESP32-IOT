f54378fea73e53831dda48570a7fecd5981c8f52
Payman22 <140825344+Payman22@users.noreply.github.com>
2024-06-05 11:43:21 +0330

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const apiKey
 const spreadsheetId = '1gzFiV8qYD5EW4fSPd1NUP2mj7h9q0-UMwNoxUagMLEU'; // Replace with your spreadsheet ID
 const sheetName = 'Sheet1'; // Replace with your sheet name
 
-fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${apiKey}`)
+fetch(`https://sheets.googleapis.com/v4/spreadsheets/${1gzFiV8qYD5EW4fSPd1NUP2mj7h9q0}/values/${sheet1}?key=${apiKey}`)
   .then(response => response.json())
   .then(data => console.log(data.values))
   .catch(error => console.error('Error:', error));
