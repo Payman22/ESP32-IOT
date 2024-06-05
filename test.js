1002f2ecf4c9c8ff3f2fec06fa6e170f0623f74b
Payman22 <140825344+Payman22@users.noreply.github.com>
2024-06-05 12:33:43 +0330

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,60 @@
-const apiKey
+const {google} = require('googleapis');
 
- =
+const serviceAccountKeyFile = "./arcane-force-342206-e7736d717b0b.json";
+const sheetId = '1gzFiV8qYD5EW4fSPd1NUP2mj7h9q0-UMwNoxUagMLEU'
+const tabName = 'sheet1'
+const range = 'A:E'
 
- 'AIzaSyAAVP254qGSX0I0IBxhfNnTQGeOlCZbc94'; // Replace with your API key
-const spreadsheetId = '1gzFiV8qYD5EW4fSPd1NUP2mj7h9q0-UMwNoxUagMLEU'; // Replace with your spreadsheet ID
-const sheetName = 'Sheet1'; // Replace with your sheet name
+main().then(() => {
+  console.log('Completed')
+})
 
-fetch(`https://sheets.googleapis.com/v4/spreadsheets/${1gzFiV8qYD5EW4fSPd1NUP2mj7h9q0}/values/${sheet1}?key=${apiKey}`)
-  .then(response => response.json())
-  .then(data => console.log(data.values))
-  .catch(error => console.error('Error:', error));
+async function main() {
+  // Generating google sheet client
+  const googleSheetClient = await _getGoogleSheetClient();
+
+  // Reading Google Sheet from a specific range
+  const data = await _readGoogleSheet(googleSheetClient, sheetId, tabName, range);
+  console.log(data);
+
+  // Adding a new row to Google Sheet
+  const dataToBeInserted = [
+     ['11', 'rohith', 'Rohith', 'Sharma', 'Active'],
+     ['12', 'virat', 'Virat', 'Kohli', 'Active']
+  ]
+  await _writeGoogleSheet(googleSheetClient, sheetId, tabName, range, dataToBeInserted);
+}
+
+async function _getGoogleSheetClient() {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: serviceAccountKeyFile,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
+  const authClient = await auth.getClient();
+  return google.sheets({
+    version: 'v4',
+    auth: authClient,
+  });
+}
+
+async function _readGoogleSheet(googleSheetClient, sheetId, tabName, range) {
+  const res = await googleSheetClient.spreadsheets.values.get({
+    spreadsheetId: sheetId,
+    range: `${tabName}!${range}`,
+  });
+
+  return res.data.values;
+}
+
+async function _writeGoogleSheet(googleSheetClient, sheetId, tabName, range, data) {
+  await googleSheetClient.spreadsheets.values.append({
+    spreadsheetId: sheetId,
+    range: `${tabName}!${range}`,
+    valueInputOption: 'USER_ENTERED',
+    insertDataOption: 'INSERT_ROWS',
+    resource: {
+      "majorDimension": "ROWS",
+      "values": data
+    },
+  })
+}
